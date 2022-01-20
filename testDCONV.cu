81e8dcd0d1596c75c1d23b702b69a66b5ff8efe6
Rudedaisy <edwardt97@yahoo.com>
2022-01-20 18:45:49 -0500

    Added tiling

--- testDCONV.cu
+++ testDCONV.cu
@@ -88,8 +88,8 @@ const int MATRIX_K = C*R*S;
 
 __device__ void fused_conv2d_im2col_and_convert(__nv_bfloat16 *data_col,
 						const float *data_im,
-						const int k, const int blockM, const int M_stride, const int blockM_index_stride,
-						const int index, const int w_col, const int h_col, const int n_col, const int n_im, const int WMMA_SUB_TILE, __nv_bfloat16 **data_col_ptr, 
+						const int k, const int blockM, const int blockM_index, const int M_stride, const int blockM_index_stride, const int tile_w,
+						const int index, const int w_col, const int h_col, const int n_col, const int n_im, const int WMMA_SUB_TILE, __nv_bfloat16 **data_col_ptr,
 						const int N, const int C, const int H, const int W, const int R, const int S, const int P, const int Q,
 						const int pad_h, const int pad_w,
 						const int stride_h, const int stride_w,
@@ -111,14 +111,20 @@ __device__ void fused_conv2d_im2col_and_convert(__nv_bfloat16 *data_col,
     const int c_im = c_col;
     const int w_im = (w_col * stride_w - pad_w) - s_displacement;
     const int h_im = (h_col * stride_h - pad_h) - r_displacement;
-
+    
     const float *data_im_ptr = data_im + (((n_im * H + h_im) * W + w_im) * C + c_im);
     
     const int M_computed = (blockDim.x * blockDim.y) / (WMMA_K*2);
+    //assert(M_computed % tile_w == 0);
+    //assert(M_computed >= tile_w);
+    int H_tile_computed = M_computed / tile_w;
+    const int tile_computed = H_tile_computed * W * C;
 
     // This if statement should not be divergent
     if (k == 0) {
-      *data_col_ptr = data_col + (((((n_col * H + h_col) * W + w_col) * C + c_col) * R + r_col) * S + s_col);
+      //*data_col_ptr = data_col + (((((n_col * H + h_col) * W + w_col) * C + c_col) * R + r_col) * S + s_col);
+      //*data_col_ptr = data_col + (((((((n_col * nth + th) * ntw + tw) * tile_h + h_colx) * tile_w + w_colx) * C + c_col) * R + r_col) * S + s_col); // Actual shape: [N, H//tile_h, W//tile_w, tile_h, tile_w, C, R, S]
+      *data_col_ptr = data_col + (((blockM_index * C + c_col) * R + r_col) * S + s_col); // Actual shape: [N, H//tile_h, W//tile_w, tile_h, tile_w, C, R, S]
     } else {
       *data_col_ptr += (WMMA_K*2);
     }
@@ -126,8 +132,11 @@ __device__ void fused_conv2d_im2col_and_convert(__nv_bfloat16 *data_col,
 #pragma unroll
     for(long long unsigned i_m = 0; i_m < (M_stride / M_computed); i_m++) {
       float val = static_cast<float>(0);
-      if (h_im > -1 && w_im > -1 && h_im < H && w_im < W){
-	val = *(data_im_ptr + i_m * M_computed * C);
+      if (h_im > -1 && w_im > -1 && (h_im + i_m*H_tile_computed) < H && w_im < W){
+	//assert((data_im_ptr + i_m * M_computed_stride * C) >= data_im);
+	//assert((data_im_ptr + i_m * M_computed_stride * C) < data_im + MATRIX_M*C);
+	//val = *(data_im_ptr + i_m * M_computed * C);
+	val = *(data_im_ptr + i_m*tile_computed);
       }
       //assert((*data_col_ptr + i_m * blockM_index_stride) >= data_col);
       //assert((*data_col_ptr + i_m * blockM_index_stride) < (data_col + MATRIX_M*MATRIX_K));
@@ -228,7 +237,7 @@ __device__ void fused_conv2d_im2col_and_BLI(float *data_col,
 //  3) Neither A nor B are transposed.
 __global__ void stages1_2_gpu_kernel(float *offset, float *deformed_columns_in,
 				     float *in, __nv_bfloat16 *columns_in, __nv_bfloat16 *weight_offset, 
-				     const int N, const int C, const int C_offset, const int H, const int W, const int R, const int S, const int P, const int Q,
+				     const int N, const int C, const int C_offset, const int H, const int W, const int R, const int S, const int P, const int Q, const int tile_h, const int tile_w,
 				     const int pad_h, const int pad_w, const int stride_h, const int stride_w, const int dilation_h, const int dilation_w,
 				     const int MATRIX_M, const int MATRIX_N, const int MATRIX_K, const int M_stride, const int N_stride
 				     ) {
@@ -250,21 +259,33 @@ __global__ void stages1_2_gpu_kernel(float *offset, float *deformed_columns_in,
 
    // Offset can now be statically allocated /// ED: In progress...
    //assert(M_stride==64 && N_stride==64);
-   //float offsetsss[64*64];
+   //__shared__ float offsetsss[128*32];
    
    // ***Constant variables for im2col function***
    // In 2D block, the order of threads is [major=y, minor=x]
    const int index = (threadIdx.y * blockDim.x + threadIdx.x);
+   const int ntw = W / tile_w;
+   const int nth = H / tile_h;
    const int blockM_index = blockM * M_stride + (index / (WMMA_K*2));
    //assert((blockDim.x * blockDim.y) % WMMA_K == 0); // Need to be divisible for "blockM_index_stride" to be accurate
    const int blockM_index_stride = MATRIX_K * (int)((blockDim.x * blockDim.y) / (WMMA_K*2));
-   const int w_col = blockM_index % W;
-   const int h_col = (blockM_index / W) % H;
-   const int n_col = (blockM_index / W / H) % N;
+   //const int w_col = blockM_index % W;
+   //const int h_col = (blockM_index / W) % H;
+   //const int n_col = (blockM_index / W / H) % N;
+   const int w_colx = blockM_index % tile_w;
+   const int h_colx = (blockM_index / tile_w) % tile_h;
+   const int tw =     (blockM_index / tile_w / tile_h) % ntw;
+   const int th =     (blockM_index / tile_w / tile_h / ntw) % nth;
+   const int w_col = tw * tile_w + w_colx;
+   const int h_col = th * tile_h + h_colx;
+   const int n_col = (blockM_index / tile_w / tile_h / ntw / nth) % N;
    const int n_im = n_col;
    const int WMMA_SUB_TILE = M_stride * (WMMA_K*2);
    __nv_bfloat16 *data_col_ptr;
 
+   //if (index == 0 && n_col==0) {
+   //  printf("BlockM %d, batch/th/tw %d/%d/%d\n", blockM, n_col, th, tw);
+   //}
 
    // Loop over k
 #pragma	unroll
@@ -283,7 +304,7 @@ __global__ void stages1_2_gpu_kernel(float *offset, float *deformed_columns_in,
 	// im2col + convert input_fp32 to columns_input_bf16
 	  fused_conv2d_im2col_and_convert(columns_in,
 					  in,
-					  i, blockM, M_stride, blockM_index_stride,
+					  i, blockM, blockM_index, M_stride, blockM_index_stride, tile_w,
 					  index, w_col, h_col, n_col, n_im, WMMA_SUB_TILE, &data_col_ptr,
 					  N, C, H, W, R, S, P, Q,
 					  pad_h, pad_w,
@@ -291,27 +312,29 @@ __global__ void stages1_2_gpu_kernel(float *offset, float *deformed_columns_in,
 					  dilation_h, dilation_w);
 	  __syncthreads();
 	}
+	///*
 	// Load the inputs
 	wmma::load_matrix_sync(a_frag, columns_in + aRow + aCol * lda, lda);
 	wmma::load_matrix_sync(b_frag, weight_offset + bRow + bCol * ldb, ldb);
 	
 	// Perform the matrix multiplication
 	wmma::mma_sync(acc_frag, a_frag, b_frag, acc_frag);
-
+	//*/
       }
    }
-   
+   ///*
    // Store result
    int cRow = warpM * WMMA_M;
    int cCol = warpN * WMMA_N;
    if (cRow < MATRIX_M && cCol < MATRIX_N) {
      wmma::store_matrix_sync(offset + cRow + cCol * ldc, acc_frag, ldc, wmma::mem_col_major);
-     //wmma::store_matrix_sync(offsetsss, acc_frag, 64, wmma::mem_col_major);
+     //wmma::store_matrix_sync(offsetsss, acc_frag, 128, wmma::mem_col_major);
    }
-  
+   
    // Perform BLI + unroll for main CONV
    const int BLI_TILE = M_stride * N_stride;
-   const float *data_offset_ptr = offset + (((n_col * H + h_col) * W + w_col) * C_offset); // C_offset = R*S*2
+   //const float *data_offset_ptr = offset + (((n_col * H + h_col) * W + w_col) * C_offset); // C_offset = R*S*2
+   const float *data_offset_ptr = offset + cRow + cCol * ldc;
    float *bli_data_col_ptr;
 #pragma unroll
    for (int k = 0; k < MATRIX_K; k += WMMA_K) {
@@ -368,6 +391,7 @@ int main(int argc, char* argv[]) {
    float *offset_fp32;
    float *deformed_columns_in_fp32;
 
+   __nv_bfloat16 *columns_in_bf16_host;
    float *offset_fp32_host;
    float *deformed_columns_in_fp32_host;
    
@@ -387,20 +411,21 @@ int main(int argc, char* argv[]) {
    int tile_w = 8;
    
    // Use tensor cores
-   cudaErrCheck(cudaMalloc((void**)&in_fp32, N*H*W*C * sizeof(float)));                           // Actual shape: [N, H//tile_h, W//tile_w, tile_h, tile_w, C]
+   cudaErrCheck(cudaMalloc((void**)&in_fp32, N*H*W*C * sizeof(float)));                           
    cudaErrCheck(cudaMalloc((void**)&weight_offset_fp32, C_offset*C*R*S * sizeof(float)));
    cudaErrCheck(cudaMalloc((void**)&columns_in_bf16, N*H*W*C*R*S * sizeof(__nv_bfloat16)));       // Actual shape: [N, H//tile_h, W//tile_w, tile_h, tile_w, C, R, S]
    cudaErrCheck(cudaMalloc((void**)&weight_offset_bf16, C_offset*C*R*S * sizeof(__nv_bfloat16)));
    cudaErrCheck(cudaMalloc((void**)&offset_fp32, N*C_offset*H*W * sizeof(float)));                // FUTURE: make temporary local array
-   cudaErrCheck(cudaMalloc((void**)&deformed_columns_in_fp32, N*H*W*C*R*S * sizeof(float)));      
-   
+   cudaErrCheck(cudaMalloc((void**)&deformed_columns_in_fp32, N*H*W*C*R*S * sizeof(float)));      // Actual shape: [N, H//tile_h, W//tile_w, tile_h, tile_w, C, R, S]      
+
+   columns_in_bf16_host = (__nv_bfloat16*)malloc(N*H*W*C*R*S * sizeof(__nv_bfloat16));
    offset_fp32_host = (float*)malloc(N*C_offset*H*W * sizeof(float));
    deformed_columns_in_fp32_host = (float*)malloc(N*H*W*C*R*S * sizeof(float));
    
    curandErrCheck(curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT));
    curandErrCheck(curandSetPseudoRandomGeneratorSeed(gen, 1337ULL));
-   
-   //curandErrCheck(curandGenerateUniform(gen, in_fp32, N*C*H*W));
+
+   //curandErrCheck(curandGenerateUniform(gen, in_fp32, N*C*H*W));                                                                                                                                                                                                          
    //curandErrCheck(curandGenerateUniform(gen, weight_offset_fp32, C_offset*C*R*S));
    curandErrCheck(curandGenerateNormal(gen, in_fp32, N*C*H*W, 0, 0.2));
    curandErrCheck(curandGenerateNormal(gen, weight_offset_fp32, C_offset*C*R*S, 0, 0.01));
@@ -416,7 +441,8 @@ int main(int argc, char* argv[]) {
    cudaErrCheck(cudaEventRecord(startConvertWeight));
    convert_fp32_bf16 <<< (C_offset*C*R*S) / 256, 256>>> (weight_offset_bf16, weight_offset_fp32, C_offset*C*R*S);
    cudaErrCheck(cudaEventRecord(stopConvertWeight));
-   
+   cudaErrCheck(cudaEventSynchronize(stopConvertWeight));
+
    // blockDim.x must be a multple of warpSize
    // 128x4 means we have 16 warps and a block computes a 64x64 output tile
    /// ED: CANNOT do 64x64 tile since N<=32. Need to halve blockDim.y
@@ -435,7 +461,7 @@ int main(int argc, char* argv[]) {
    
    stages1_2_gpu_kernel <<< gridDim, blockDim >>> (offset_fp32, deformed_columns_in_fp32,
 						   in_fp32, columns_in_bf16, weight_offset_bf16,
-						   N, C, C_offset, H, W, R, S, P, Q,
+						   N, C, C_offset, H, W, R, S, P, Q, tile_h, tile_w,
 						   pad_h, pad_w, stride_h, stride_w, dilation_h, dilation_w,
 						   MATRIX_M, MATRIX_N, MATRIX_K, M_stride, N_stride);
    cudaErrCheck(cudaEventRecord(stopWMMA));
@@ -457,8 +483,24 @@ int main(int argc, char* argv[]) {
 
    
    printf("\nChecking results...\n");
+   cudaErrCheck(cudaMemcpy(columns_in_bf16_host, columns_in_bf16, N*H*W*C*R*S * sizeof(__nv_bfloat16), cudaMemcpyDeviceToHost));
    cudaErrCheck(cudaMemcpy(offset_fp32_host, offset_fp32, N*C_offset*H*W * sizeof(float), cudaMemcpyDeviceToHost));
    cudaErrCheck(cudaMemcpy(deformed_columns_in_fp32_host, deformed_columns_in_fp32, N*H*W*C*R*S * sizeof(float), cudaMemcpyDeviceToHost));
+
+   /*
+   printf("Unrolled input:\n");
+   for (int i = 0; i < MATRIX_M; i++) {
+     for (int j = 0; j < MATRIX_K; j++) {
+       float val = __bfloat162float(columns_in_bf16_host[i*MATRIX_K + j]);
+       //printf("%.3f ", val);
+       if (val == 0) {
+         printf("\nZero detected at idx=%d N/H/W = %d/%d/%d, C/R/S = %d/%d/%d\n", i, (i/W/H)%N, (i/W)%H, i%W, (j/R/S)%C, (j/S)%R, j%S);
+	 // break;
+       }
+     }
+     //printf("\n");
+   }
+   //*/
    
    /*
    unsigned reasonable = 0;
@@ -468,10 +510,10 @@ int main(int argc, char* argv[]) {
        for (int j = 0; j < MATRIX_N; j++) {
        	   float v1 = offset_fp32_host[i*MATRIX_N + j];
        	   //printf("%.3f ", v1);
-	   //if (v1 == 0) {
-	   //  printf("Zero detected at idx=%d N/H/W = %d/%d/%d, K = %d\n", i, (i/W/H)%N, (i/W)%H, i%W, j);
-	   //  exit(0);
-	   //}
+	   if (v1 == 0) {
+	     printf("Zero detected at idx=%d N/H/W = %d/%d/%d, K = %d\n", i, (i/W/H)%N, (i/W)%H, i%W, j);
+	     break;
+	   }
 	   if (v1 < 10) {
 	     //printf("Reasonable offset detected at idx=%d N/H/W = %d/%d/%d, K = %d. VALUE = %f\n", i, (i/W/H)%N, (i/W)%H, i%W, j, v1);
 	     reasonable++;
